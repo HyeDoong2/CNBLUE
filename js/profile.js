d93715897c51308493aa97d283ed1a01c659ed60
hhj <gkagus98@naver.com>
2026-02-26 17:35:10 +0900

    260226

--- js/profile.js
+++ js/profile.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 
-  // Member Hover Image Change
+  // 호버 시 이미지 변경
   const members = document.querySelectorAll(".member");
 
   members.forEach((member, index) => {
@@ -43,5 +43,54 @@ document.addEventListener("DOMContentLoaded", function () {
       section.classList.toggle("active");
     });
   });
+
+
+  // 현재 페이지 메뉴 active
+const currentPage = window.location.pathname.split("/").pop();
+const menuLinks = document.querySelectorAll(".gnb ul li a");
+
+menuLinks.forEach(link => {
+  const linkPage = link.getAttribute("href");
+
+  if (linkPage === currentPage) {
+    link.classList.add("active");
+  }
+});
+
+// 스크롤 등장 (member)
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+members.forEach(member => {
+  observer.observe(member);
+});
+
+
+// 스크롤 등장 (AWARDS / CONCERT)
+const sections = document.querySelectorAll(".scroll-section");
+
+function checkScroll() {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  sections.forEach(function (section) {
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < triggerBottom) {
+      section.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", checkScroll);
+checkScroll();
+
+console.log(sections);
 
 });
